4f2f9f425cc6555ef0f742955f4cec32027640a7
Synthesizer <65801316+tunlaton11@users.noreply.github.com>
2020-11-06 16:52:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+  <title>63070224</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+
+<body style="background-color:#FFF1C4 ;">
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto col-md-8 text-center">
+          <h1>63070224</h1>
+          <p class="mb-4">Tunlaton Wongchai</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-6 p-2">
+          <div class="card h-10"></div>
+          <div class="card"> <img class="card-img-top" src="img/life/life2.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"><b>Test1</b></h5>
+              <p class="card-text">Test1</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-6 p-2">
+          <div class="card h-10"></div>
+          <div class="card"> <img class="card-img-top" src="img/life/life3.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"> <b>Test2</b></h5>
+              <p class="card-text">Test2</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-6 p-2">
+          <div class="card"> <img class="card-img-top" src="img/life/life4.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title"> <b>Test3</b></h5>
+              <p class="card-text">Test3<br></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</body>
+
+</html>
