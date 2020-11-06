9771bbd4fe6734f7cfeb829034741ee2f6cdecd4
Synthesizer <65801316+tunlaton11@users.noreply.github.com>
2020-11-06 16:57:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css"/>
 </head>
 
-<body style="background-color:#757575 ;">
+<body style="background-color:#581845 ;">
   <div class="py-5">
     <div class="container">
       <div class="row">
@@ -23,8 +23,8 @@
           <div class="card h-10"></div>
           <div class="card"> <img class="card-img-top" src="img/life/life2.jpg" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title"><b>Test1</b></h5>
-              <p class="card-text">Test1</p>
+              <h5 class="card-title"><b>Card 1</b></h5>
+              <p class="card-text">I love ITF</p>
             </div>
           </div>
         </div>
@@ -32,16 +32,16 @@
           <div class="card h-10"></div>
           <div class="card"> <img class="card-img-top" src="img/life/life3.jpg" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title"> <b>Test2</b></h5>
-              <p class="card-text">Test2</p>
+              <h5 class="card-title"> <b>Card 2</b></h5>
+              <p class="card-text">I need A ITF</p>
             </div>
           </div>
         </div>
         <div class="col-lg-4 col-6 p-2">
           <div class="card"> <img class="card-img-top" src="img/life/life4.jpg" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title"> <b>Test3</b></h5>
-              <p class="card-text">Test3<br></p>
+              <h5 class="card-title"> <b>Card 3</b></h5>
+              <p class="card-text">I love Web Tech<br></p>
             </div>
           </div>
         </div>
